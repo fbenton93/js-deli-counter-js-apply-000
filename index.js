b9996364bf2480a4eb730850f4619483a67361c4
Forrest Benton <elliottbenton@gmail.com>
2018-07-17 14:17:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,25 +6,24 @@ function takeANumber(katzDeliLine,newPerson) {
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
+  if (!katzDeliLine.length) {
     return "There is nobody waiting to be served!";
   } else {
-    var nextPerson = katzDeliLine.shift();
-    return `Currently serving ${nextPerson}.`;
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
 
 function currentLine(katzDeliLine) {
-  var output = [];
+  var output = "";
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   }
   for (let i = 0; i < katzDeliLine.length; i++) {
     output += (i + 1) + ". " + katzDeliLine[i] + ", ";
   }
-  var finalOutput = output.slice(0,-2);
   
-  return "The line is currently: " + finalOutput;
+  
+  return "The line is currently: " + output.slice(0,-2);
 }
 
 
