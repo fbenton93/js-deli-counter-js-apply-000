62fc47cbf57c953af79d40506ca1eca6dcd31bbf
Forrest Benton <elliottbenton@gmail.com>
2018-07-17 12:55:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine,newPerson) {
-  katzDeliLine.push(newPerson)
-  return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`
+  katzDeliLine.push(newPerson);
+  return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`;
 }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
   } else {
-    var nextPerson = katzDeliLine.shift()
-    return `Currently serving ${nextPerson}.`
+    var nextPerson = katzDeliLine.shift();
+    return `Currently serving ${nextPerson}.`;
   }
 }
 
 function currentLine(katzDeliLine) {
   var output = [];
   if (katzDeliLine.length === 0) {
-    return "The line is currently empty."
+    return "The line is currently empty.";
   }
   for (let i = 0; i < katzDeliLine.length; i++) {
-    output += (i + 1) + ". " + katzDeliLine[i] + ", "
+    output += (i + 1) + ". " + katzDeliLine[i] + ", ";
   }
-  var finalOutput = output.slice(0,-2)
+  var finalOutput = output.slice(0,-2);
   
-  return "The line is currently: " + finalOutput
+  return "The line is currently: " + finalOutput;
 }
 
 
